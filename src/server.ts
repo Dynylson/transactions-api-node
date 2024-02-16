123f5dbc5c5e15f9710df7fbbab4d0b2e9af0f5b
Dynylson Júnior <dynylsonjr@gmail.com>
2024-02-16 18:23:03 -0300

    test: should be able to list all transactions

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,6 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
+import { app } from './app'
 
-const app = fastify()
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-  prefix: '/transactions',
+app.listen({ port: env.PORT }, () => {
+  console.log(`Server is running on port ${env.PORT}`)
 })
-
-app.get('/hello', () => {
-  return 'Hello World'
-})
-
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('HTTP Server Running!')
-  })
--- test/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('o usuário consegue criar uma nova transacão', () => {
-  const responseStatusCode = 201
-
-  expect(responseStatusCode).toEqual(201)
-})
